8f34291a418a58da7d6fbfcfc3c7c7d6ed5d2c18
linzhengen <linzhengen@yahoo.co.jp>
2024-06-17 08:14:56 +0900

    feat(kotlin): impl cleanarchspring

--- kotlin/cleanarch-spring/build.gradle.kts
+++ kotlin/cleanarch-spring/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("org.flywaydb.flyway") version "10.15.0"
+	id("com.diffplug.spotless") version "6.25.0"
 	kotlin("plugin.jpa") version "1.9.24"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
